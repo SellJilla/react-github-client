2bc41981b942501e51ef34ffa8369ba8a51d1813
SellJilla <sell.jilla@protonmail.com>
2022-06-30 15:41:55 +0300

    redirect from index to repos

--- src/components/Content.js
+++ src/components/Content.js
@@ -2,15 +2,16 @@ import React from 'react'
 import {
   Routes,
   Route,
+  Navigate
 } from 'react-router-dom';
 import About from './About';
 
 const Content = () => {
   return (
     <Routes>
-      <Route index element={<About />}/>
-      <Route path='/about' element={<About />}/>
-      <Route path='/repositories' element={<About />} />
+      <Route index element={<Navigate to='/repositories' replace={ true } />} />
+      <Route path='/about' element={<About />} />
+      <Route path='/repositories' element={<div>Repositories</div>} />
     </Routes>
   )
 }
