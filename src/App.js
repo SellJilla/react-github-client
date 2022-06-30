dda3ccd6c3e4f80a059ce20fbc3e5149b732e28e
SellJilla <sell.jilla@protonmail.com>
2022-06-30 14:46:43 +0300

    add initial layout template for the app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import Content from './components/Content';
+import Header from './components/Header';
+import SideMenu from './components/SideMenu';
+import UserInfo from './components/UserInfo';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App container'>
+      <div className='row vh-100'>
+        <div className='col-3'>
+          <div className='py-5'>
+            <UserInfo />
+          </div>
+          <SideMenu />
+        </div>
+        <div className='col-9'>
+          <div className='py-5'>
+            <Header />
+          </div>
+          <Content />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Content.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Content = () => {
+  return (
+    <div className='content container-fluid'>Content</div>
+  )
+}
+
+export default Content
